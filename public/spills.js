dbdfa1e2add5d66916fe7e1e475f6393692525cd
Dan Tennery-Spalding <teachrdan@gmail.com>
2015-01-20 16:51:14 -0800

    Works, now with company name attached to mouse tooltip.

--- public/spills.js
+++ public/spills.js
@@ -31,7 +31,6 @@ d3.json("./us.json", function(error, us) {
       .data(topojson.feature(us, us.objects.states).features)
     .enter().append("path")
       .attr("d", path)
-      .on("click", clicked);
 
 g.append("path")
   .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
@@ -41,22 +40,54 @@ g.append("path")
 
 ///////////////////////////////////////////////////////////////////
 
-var spillData = d3.csv("./spills2.csv")
+// function testGPS(coordinates) {
+//   var x = coordinates[0], y = coordinates[1];
+//   point = null;
+//   (lower48Point(x, y), point) || (alaskaPoint(x, y), point) || hawaiiPoint(x, y);
+//   return point;
+// }
+
+// .attr('test', function(d) { if (testGPS([d.long, d.lat]) === null) {
+//   console.log('the error is', data);
+// }
+// });
+
+///////////////////////////////////////////////////////////////////
+
+
+var spillData = d3.csv("./spills3.csv")
   .row(function(d) {
     return {
       report_num: +d.report_num,
-      significant: +d.significant,
-      long: +d.long,
-      lat: +d.lat
+      report_received: d.report_received,
+      co_name: d.co_name,
+      significant: d.significant,
+      lat: +d.lat,
+      long: +d.long
     };
   })
   .get(function callback(error, rows) {
-    console.log('rows', rows[0]);
+    console.log('rows[0]', rows[0]);
     circles.selectAll('.spills')
       .data(rows)
       .enter().append('svg:circle')
-        .attr('class', function(d) { return 'sig' + d.significant; })
-        .attr('r', r)
-        .attr('cx', function(d) { return projection([d.long, d.lat])[0]; })
-        .attr('cy', function(d) { return projection([d.long, d.lat])[1]; })
-  });
+        .attr('class', function(d) { if (d.significant === 'true') { return 'sig' + 1; }
+          else { return 'sig' + 0 }
+        })
+        .attr('r', function(d) { 
+          if (d.significant === 1) {
+          return r+1;
+        } else {
+          return r;
+        }
+        })
+        .attr('test', function(d) {
+          if (projection([d.long, d.lat]) === null) {
+            console.log('bad data', d);
+          }
+        })
+    .attr('cx', function(d) { return projection([d.long, d.lat])[0]; })
+    .attr('cy', function(d) { return projection([d.long, d.lat])[1]; })
+    .append("svg:title")
+    .text(function(d) { return d.co_name; })
+});
