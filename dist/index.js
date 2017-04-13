46215062ac875c9aab27a7dd971ec4857aba3781
Dan Tennery-Spalding <teachrdan@gmail.com>
2017-04-12 17:01:00 -0700

    experimenting

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+var app = require('./server.js');
+
+app.listen(8000);
---
+++ dist/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+var app = express();
+
+mongoose.connect('mongodb://localhost/spills');
+
+// configure our server with all the middleware and and routing
+// require('middleware')(app, express);
+
+// export our app for testing and flexibility, required by index.js
+module.exports = app;
+
